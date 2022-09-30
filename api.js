449009590791eefffdcd0d0968a087bcca60ac1e
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-09-29 22:33:16 -0500

    Agrega parametro de options de validate y stats a la funcion mdLinks

--- api.js
+++ api.js
@@ -22,12 +22,13 @@ const functions = require('./index.js');
 const ruta = './example/exampleFile.md';
 const ruta2 = '../img/img.jpg';
 const ruta3 = './example/index.html';
+const ruta4 = './example/readmeVacio.md';
 const rutaAbsoluta = 'C:\\Users\\USUARIO\\laboratoria\\prueba';
 const rutaAbs1 = 'C:\\Users\\USUARIO\\laboratoria\\LIM018-md-links\\example\\exampleFile.md';
 
 
 
-function mdLinks(path) { //, { validate: false, stats: false }) {
+function mdLinks(path, options = { validate: false, stats: false }) {
   const promiseMdLinks = new Promise((resolve, reject) => {
     if ( !functions.pathExists(path)) {
       reject('La ruta no existe');
@@ -38,21 +39,32 @@ function mdLinks(path) { //, { validate: false, stats: false }) {
     if(!functions.mdExtension(path)){
       reject('No es un archivo .md');
     }
-    const contentFile = functions.readFile(path);  
-    // resolve('convierte tu ruta a absoluta');
-    // reject('Message: La ruta no existe');
-    console.log(new String(validatePath).valueOf());
-    console.log(new String(path).valueOf());
+    const contentFile = functions.readFile(path);
+    const arrayLinks = functions.saveArrayLinks(contentFile);
+    const objLinks = functions.objecLinks(arrayLinks, path);
+    const statusPrommise = functions.statusHTTP(objLinks);
+    const uniqueLinks = functions.statsUnique(objLinks);
+    const brokenLinks = functions.statsBroken(objLinks);
+    if(options.validate && options.stats == false){
+      resolve(statusPrommise);
+    }
+    if(options.stats && options.validate == false){
+      resolve(uniqueLinks);
+    }
+    if(options.stats && options.validate){
+      // resolve(uniqueLinks);
+      resolve(`${uniqueLinks}
+      ${brokenLinks}`);
+    }
+    resolve(objLinks);
   });
-  // console.log(promiseMdLinks);
   return promiseMdLinks;
 }
 
-mdLinks(ruta)
+mdLinks(ruta, { validate: true, stats: true })
   .then((res) => {
     console.log(res);
   })
   .catch((err) => {
     console.log(err);
   });
-
