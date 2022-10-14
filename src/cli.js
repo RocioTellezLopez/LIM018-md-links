f9d6cd6c84b804f84dfe5b66c4f2573c79b192f3
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-10-13 23:56:34 -0500

    Agrega estilo y color a los console.log del comando md-links

--- src/cli.js
+++ src/cli.js
@@ -11,18 +11,66 @@ const argv = process.argv;
 // console.log(argv);
 const options = {validate: argv.includes('--validate'), stats: argv.includes('--stats')};
 
+
+
+if(argv.length === 2) {
+  console.log(`
+    ███╗   ███╗██████╗       ██╗     ██╗███╗   ██╗██╗  ██╗███████╗
+    ████╗ ████║██╔══██╗      ██║     ██║████╗  ██║██║ ██╔╝██╔════╝
+    ██╔████╔██║██║  ██║█████╗██║     ██║██╔██╗ ██║█████╔╝ ███████╗
+    ██║╚██╔╝██║██║  ██║╚════╝██║     ██║██║╚██╗██║██╔═██╗ ╚════██║
+    ██║ ╚═╝ ██║██████╔╝      ███████╗██║██║ ╚████║██║  ██╗███████║
+    ╚═╝     ╚═╝╚═════╝       ╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
+                                                                  
+    `);
+
+}
+
+if(argv.length === 3 && argv.includes(route)) {
+  mdLinks(route, options)
+    .then((resArrPromise) => {
+    
+      // console.log('promesa: =>', resArrPromise.flat());
+      const objPromise = resArrPromise.flat();
+      
+      console.log('◈◇◈◇◈◇◈◇ MD-LINKS by Rocio Tellez ◈◇◈◇◈◇◈◇◈');
+
+      objPromise.forEach(objLinks => {
+        console.log('◈◇◈◇◈◇◈◇ Links Encontrados ◈◇◈◇◈◇◈◇◈');
+
+        console.log(chalk.blue('Text:'), objLinks.text);
+        console.log(chalk.blue('Href:'), objLinks.href);
+        console.log(chalk.blue('File:'), objLinks.file);
+
+        // console.log(objLinks);
+
+      });
+      // resArrPromise.forEach(elementPromise => {
+      //   elementPromise.then((resPromise) => console.log(resPromise));
+      
+    // });
+    })
+    .catch((err) => {
+      console.log('err: ', err);
+    });
+}
+  
 // console.log(options);
 // console.log(chalk.blue(`Este es la ruta ingresada: ${route} ${options}`));
 
+// inicio 
 
-mdLinks(route, options)
-  .then((resArrPromise) => {
-    console.log('promesa: =>', resArrPromise);
-    // resArrPromise.forEach(elementPromise => {
-    //   elementPromise.then((resPromise) => console.log(resPromise));
+
+
+// mdLinks(route, options)
+//   .then((resArrPromise) => {
+    
+//     console.log('promesa: =>', resArrPromise);
+//     // resArrPromise.forEach(elementPromise => {
+//     //   elementPromise.then((resPromise) => console.log(resPromise));
       
-    // });
-  })
-  .catch((err) => {
-    console.log('err: ', err);
-  });
+//     // });
+//   })
+//   .catch((err) => {
+//     console.log('err: ', err);
+//   });
